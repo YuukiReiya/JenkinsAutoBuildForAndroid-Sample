1519a7e2b31ec7db8739cb743115b6185d11303d
Yuuki Bamba <yuukiBamba@gmail.com>
2019-12-05 09:59:12 +0900

    1st commit! add project file.

---
+++ JenkinsAutoBuild Sample/Assets/Editor/TestBuilder.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class TestBuilder
+{
+    // ビルド実行でAndroidのapkを作成する例
+    [MenuItem("Tools/Build Project AllScene Android")]
+    public static void BuildProjectAllSceneAndroid()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android,BuildTarget.Android);
+        List<string> allScene = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled)
+            {
+                allScene.Add(scene.path);
+            }
+        }
+        PlayerSettings.applicationIdentifier = "com.yourcompany.newgame";
+        PlayerSettings.statusBarHidden = true;
+        BuildPipeline.BuildPlayer(
+            allScene.ToArray(),
+            "C:\\Users\\yuuki\\Documents\\Develop\\bin\\test.apk",
+            BuildTarget.Android,
+            BuildOptions.None
+        );
+    }
+}
